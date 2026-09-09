3d896af41fa606e20391b31060e5af80118c1a0b
Zirluka <kopaflopa@gmail.com>
2026-09-09 21:50:25 +0400

    DB update

--- app/Models/Click.php
+++ app/Models/Click.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Click extends Model
 {
-    //
+    protected $fillable = [ "link_id", "ip", "country", "city", "referer", "user_agent", "device_type", "os", "browser", "clicked_at" ];
+
+    public function link() {
+        return $this->belongsTo(Link::class);
+    }
+
 }
--- app/Models/Link.php
+++ app/Models/Link.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
-    //
+    protected $fillable = [ "user_id", "original_url", "short_code", "password", "expired_at", "is_active", "clicks_count" ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
 }
