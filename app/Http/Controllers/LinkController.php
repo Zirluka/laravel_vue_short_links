c7f05a26255dd16102d1f4b451c315d9f461467b
Zirluka <kopaflopa@gmail.com>
2026-09-11 02:30:34 +0400

    Get link logic

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -4,11 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LinkRequest;
 use App\Http\Services\ShortLinkService;
+use App\Models\Link;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LinkController extends Controller
 {
+
+    // index
+    public function getUrl($code) {
+        $link = Link::where("short_code", $code)->first();
+        if (!$link) {
+            return response()->json([], 404);
+        }
+
+        return response()->json([
+            "data" => $link->original_url
+        ], 200);
+    }
+
+
+    // create
     public function shortLink(LinkRequest $request, ShortLinkService $shortLinkService) {
         // Если пользователь не авторизован, то создаем анонимную ссылку
         if (!Auth::check()) {
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 // API ROUTES
 Route::post("/", [LinkController::class, 'shortLink']);
+
+Route::get("/{code}", [LinkController::class, 'getUrl']);
