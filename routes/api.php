f7cdbb18193dc3c9c12b9f17ca6d4024cb368c19
Zirluka <kopaflopa@gmail.com>
2026-09-11 20:45:34 +0400

    breeze updates

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
-use App\Http\Controllers\LinkController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LinkController;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 // API ROUTES
 Route::post("/", [LinkController::class, 'shortLink']);
-
 Route::get("/{code}", [LinkController::class, 'getUrl']);
+
+require __DIR__.'/auth.php';
