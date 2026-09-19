f20a0a8b1d526095c2befbd9e072f380827b2eef
Zirluka <kopaflopa@gmail.com>
2026-09-19 18:40:19 +0400

    UPDATE: add UserController routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LinkController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', [UserController::class, 'getUser']);
+    Route::patch('/user', [UserController::class, 'update']);
+    Route::delete('/user', [UserController::class, 'destroy']);
 });
 
 // API ROUTES
