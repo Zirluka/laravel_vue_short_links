c8c2a22542e31539e7ea4e53df560ebedf454dcd
Zirluka <kopaflopa@gmail.com>
2026-09-26 16:49:10 +0400

    PREPARE: deploy prepares

--- config/location.php
+++ config/location.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'driver' => IpApi::class,
+    'driver' => Stevebauman\Location\Drivers\MaxMind::class,
 
     /*
     |--------------------------------------------------------------------------
@@ -100,19 +100,12 @@ return [
     */
 
     'maxmind' => [
-        'license_key' => env('MAXMIND_LICENSE_KEY'),
-
         'web' => [
             'enabled' => false,
-            'user_id' => env('MAXMIND_USER_ID'),
-            'locales' => ['en'],
-            'options' => ['host' => 'geoip.maxmind.com'],
         ],
-
         'local' => [
             'type' => 'city',
-            'path' => database_path('maxmind/GeoLite2-City.mmdb'),
-            'url' => sprintf('https://download.maxmind.com/app/geoip_download_by_token?edition_id=GeoLite2-City&license_key=%s&suffix=tar.gz', env('MAXMIND_LICENSE_KEY')),
+            'path' => database_path('geoip/GeoLite2-City.mmdb'),
         ],
     ],
 
