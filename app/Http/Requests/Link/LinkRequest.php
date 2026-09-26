eff404bc392fd6e3c7d8401eb9b5e21f442a5217
Zirluka <kopaflopa@gmail.com>
2026-09-26 19:05:42 +0400

    HOTFIX: links fix

--- app/Http/Requests/Link/LinkRequest.php
+++ app/Http/Requests/Link/LinkRequest.php
@@ -3,8 +3,6 @@
 namespace App\Http\Requests\Link;
 
 use App\Rules\NotSelfDomain;
-use App\Rules\ReachableUrl;
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class LinkRequest extends FormRequest
@@ -12,7 +10,7 @@ class LinkRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'link' => ['required', 'url:http,https', 'string', 'max:2048', new NotSelfDomain(), new ReachableUrl()],
+            'link' => ['required', 'url:http,https', 'string', 'max:2048', new NotSelfDomain()],
             'password' => ['sometimes', 'nullable', 'string', 'min:4'],
             'expired_at' => ['sometimes', 'nullable', 'date', 'after:now']
         ];
